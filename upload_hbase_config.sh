13fc5f6016f5f6dd318c7052a9cbf07c8fd62275
daniosim <dani.sim@tamr.com>
2020-09-08 17:08:07 -0400

    Add SSE to config sync.

--- upload_hbase_config.sh
+++ upload_hbase_config.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Sync contents of /etc/hbase/conf.dist directory to s3
-aws s3 sync /etc/hbase/conf.dist/ s3://${emr_hbase_s3_bucket_root_dir}/${hbase_config_path}
+aws s3 sync /etc/hbase/conf.dist/ s3://${emr_hbase_s3_bucket_root_dir}/${hbase_config_path} --sse AES256
 
 # Sync contents of /etc/hadoop/conf directory to s3
-aws s3 sync /etc/hadoop/conf/ s3://${emr_hbase_s3_bucket_root_dir}/${hadoop_config_path}
+aws s3 sync /etc/hadoop/conf/ s3://${emr_hbase_s3_bucket_root_dir}/${hadoop_config_path} --sse AES256
