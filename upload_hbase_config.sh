322cbebf7e74ad34a988933d68ba7efe6b9b8e10
daniosim <dani.sim@tamr.com>
2020-10-07 20:04:26 -0400

    DEV-14281 Remove testing lines in upload_hbase_config.sh.

--- upload_hbase_config.sh
+++ upload_hbase_config.sh
@@ -5,10 +5,8 @@ app=${1}
 
 if [[ $app == 'hbase' ]]; then
     # Sync contents of /etc/hbase/conf.dist directory to s3
-    echo "Sync hbase config"
-    # aws s3 sync /etc/hbase/conf.dist/ s3://${bucket_name_for_root_directory}/${hbase_config_path} --sse AES256
+    aws s3 sync /etc/hbase/conf.dist/ s3://${bucket_name_for_root_directory}/${hbase_config_path} --sse AES256
 fi
 
 # Sync contents of /etc/hadoop/conf directory to s3
-# aws s3 sync /etc/hadoop/conf/ s3://${bucket_name_for_root_directory}/${hadoop_config_path} --sse AES256
-echo 'Sync hadoop config'
+aws s3 sync /etc/hadoop/conf/ s3://${bucket_name_for_root_directory}/${hadoop_config_path} --sse AES256
