f2e26d3b73268e29245aa871ff0378c68d59f9c0
Dan Souza <dan.souza@tamr.com>
2021-01-08 12:03:41 -0500

    Bump version to 0.12.0

--- examples/static-spark/main.tf
+++ examples/static-spark/main.tf
@@ -30,7 +30,7 @@ module "emr_key_pair" {
 
 # EMR Static Spark cluster
 module "emr-spark" {
-  # source = "git::git@github.com:Datatamer/terraform-aws-emr.git?ref=0.11.1"
+  # source = "git::git@github.com:Datatamer/terraform-aws-emr.git?ref=0.12.0"
   source = "../.."
 
   # Configurations
