a9da577072093ffaab70ee0defcab00083a99f7f
Hugo Barros <hugo.barros@caylent.com>
2021-08-04 15:28:41 -0300

    removing hardcoded vars in test_examples

--- test_examples/test_module_emr-iam/main.tf
+++ test_examples/test_module_emr-iam/main.tf
@@ -1,12 +1,12 @@
 module "emr-iam" {
   source = "../../modules/aws-emr-iam"
 
-  s3_bucket_name_for_logs           = "bucket-name-logs"                             // var.s3_bucket_name_for_logs
-  s3_bucket_name_for_root_directory = "bucket-name-root"                             // var.s3_bucket_name_for_root_directory
-  s3_policy_arns                    = ["arn:aws:iam::aws:policy/AmazonS3FullAccess"] // var.s3_policy_arns
-  emr_ec2_iam_policy_name           = "hbase-terratest-ec2-policy"                   // var.emr_ec2_iam_policy_name
-  emr_service_iam_policy_name       = "hbase-terratest-service-policy"               // var.emr_service_iam_policy_name
-  emr_service_role_name             = "hbase-terratest-service-role"                 // var.emr_service_role_name
-  emr_ec2_instance_profile_name     = "hbase-terratest-instance-profile"             // var.emr_ec2_instance_profile_name
-  emr_ec2_role_name                 = "hbase-terratest-ec2-role"                     // var.emr_ec2_role_name
+  s3_bucket_name_for_logs           = var.s3_bucket_name_for_logs
+  s3_bucket_name_for_root_directory = var.s3_bucket_name_for_root_directory
+  s3_policy_arns                    = var.s3_policy_arns
+  emr_ec2_iam_policy_name           = var.emr_ec2_iam_policy_name
+  emr_service_iam_policy_name       = var.emr_service_iam_policy_name
+  emr_service_role_name             = var.emr_service_role_name
+  emr_ec2_instance_profile_name     = var.emr_ec2_instance_profile_name
+  emr_ec2_role_name                 = var.emr_ec2_role_name
 }
