42f64375a7691ec1f5219471f1a28cd7a93e45c7
Hugo Barros <hugo.barros@caylent.com>
2021-09-10 01:16:50 -0300

    removing region from iam setup

--- test/create_module_emr-iam_test.go
+++ test/create_module_emr-iam_test.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -26,17 +25,10 @@ func TestModuleEmrIAM(t *testing.T) {
 		"emr_ec2_role_name":                 fmt.Sprintf("%s-%s", uniqueId, "terratest-ec2-role"),
 	}
 
-	// Getting a random region between the US ones
-	awsRegion := aws.GetRandomRegion(t, []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"}, nil)
-
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: "../test_examples/module_emr-iam",
 		Vars:         testVars,
-		// Environment variables to set when running Terraform
-		EnvVars: map[string]string{
-			"AWS_DEFAULT_REGION": awsRegion,
-		},
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
