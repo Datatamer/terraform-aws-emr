99014fd05b042246029eb6f9c581d46eaa97f195
Hugo Barros <hugo.barros@caylent.com>
2021-09-10 00:16:28 -0300

    fixing iam module tests

--- test/create_module_emr-iam_test.go
+++ test/create_module_emr-iam_test.go
@@ -1,9 +1,12 @@
 package test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,13 +14,25 @@ import (
 func TestModuleEmrIAM(t *testing.T) {
 	t.Parallel()
 
+	uniqueId := strings.ToLower(random.UniqueId())
+	testVars := map[string]interface{}{
+		"s3_bucket_name_for_logs":           fmt.Sprintf("%s-%s", uniqueId, "emr-terratest-logs"),
+		"s3_bucket_name_for_root_directory": fmt.Sprintf("%s-%s", uniqueId, "emr-terratest-root"),
+		"s3_policy_arns":                    []string{"arn:aws:iam::aws:policy/AmazonS3FullAccess"},
+		"emr_ec2_iam_policy_name":           fmt.Sprintf("%s-%s", uniqueId, "terratest-ec2-policy"),
+		"emr_service_iam_policy_name":       fmt.Sprintf("%s-%s", uniqueId, "terratest-service-policy"),
+		"emr_service_role_name":             fmt.Sprintf("%s-%s", uniqueId, "terratest-service-role"),
+		"emr_ec2_instance_profile_name":     fmt.Sprintf("%s-%s", uniqueId, "terratest-instance-profile"),
+		"emr_ec2_role_name":                 fmt.Sprintf("%s-%s", uniqueId, "terratest-ec2-role"),
+	}
+
 	// Getting a random region between the US ones
 	awsRegion := aws.GetRandomRegion(t, []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"}, nil)
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../test_examples/test_module_emr-iam",
-
+		TerraformDir: "../test_examples/module_emr-iam",
+		Vars:         testVars,
 		// Environment variables to set when running Terraform
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": awsRegion,
--- test_examples/module_emr-iam/main.tf
+++ test_examples/module_emr-iam/main.tf
@@ -1,12 +1,13 @@
 module "emr-iam" {
   source = "../../modules/aws-emr-iam"
 
-  s3_bucket_name_for_logs           = "bucket-name-logs"
-  s3_bucket_name_for_root_directory = "bucket-name-root"
-  s3_policy_arns                    = ["arn:aws:iam::aws:policy/AmazonS3FullAccess"]
-  emr_ec2_iam_policy_name           = "hbase-terratest-ec2-policy"
-  emr_service_iam_policy_name       = "hbase-terratest-service-policy"
-  emr_service_role_name             = "hbase-terratest-service-role"
-  emr_ec2_instance_profile_name     = "hbase-terratest-instance-profile"
-  emr_ec2_role_name                 = "hbase-terratest-ec2-role"
+  s3_bucket_name_for_logs           = var.s3_bucket_name_for_logs
+  s3_bucket_name_for_root_directory = var.s3_bucket_name_for_root_directory
+  s3_policy_arns                    = var.s3_policy_arns
+  emr_ec2_iam_policy_name           = var.emr_ec2_iam_policy_name
+  emr_service_iam_policy_name       = var.emr_service_iam_policy_name
+  emr_service_role_name             = var.emr_service_role_name
+  emr_ec2_instance_profile_name     = var.emr_ec2_instance_profile_name
+  emr_ec2_role_name                 = var.emr_ec2_role_name
+  vpc_id                            = "vpc-1234567890123456"
 }
