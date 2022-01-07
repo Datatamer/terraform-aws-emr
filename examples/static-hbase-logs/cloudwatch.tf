7d3bf2f43660004be4bc2de1ca470e3ef6ec8071
Federico Vazquez <federico.vazquez@caylent.com>
2022-01-07 17:25:15 -0300

    Adds path.module reference.

--- examples/static-hbase-logs/cloudwatch.tf
+++ examples/static-hbase-logs/cloudwatch.tf
@@ -7,6 +7,6 @@ resource "aws_cloudwatch_log_group" "tamr_log_group" {
 data "aws_region" "current" {}
 
 resource "local_file" "postfix_config" {
-  filename = "./files/cloudwatch-install.sh"
-  content  = templatefile("./files/cloudwatch-install.tpl", { region = data.aws_region.current.name, endpoint = module.endpoints.endpoints["logs"].dns_entry[0]["dns_name"], log_group = aws_cloudwatch_log_group.tamr_log_group.name })
+  filename = "${path.module}/files/cloudwatch-install.sh"
+  content  = templatefile("${path.module}/files/cloudwatch-install.tpl", { region = data.aws_region.current.name, endpoint = module.endpoints.endpoints["logs"].dns_entry[0]["dns_name"], log_group = aws_cloudwatch_log_group.tamr_log_group.name })
 }
