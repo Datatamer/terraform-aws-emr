be732b92faba6b1badc5fd918f2db20465f5bb9d
Federico Vazquez <federico.vazquez@caylent.com>
2022-01-11 20:31:30 -0300

    Squashs

---
+++ examples/static-hbase-logs/networking.tf
@@ -0,0 +1,40 @@
+module "sg-ports" {
+  # source               = "git::https://github.com/Datatamer/terraform-aws-emr.git//modules/aws-emr-ports?ref=6.2.0"
+  source       = "../../modules/aws-emr-ports"
+  applications = local.this_application
+}
+
+module "aws-emr-sg-master" {
+  source                  = "git::git@github.com:Datatamer/terraform-aws-security-groups.git?ref=1.0.0"
+  vpc_id                  = var.vpc_id
+  ingress_cidr_blocks     = var.ingress_cidr_blocks
+  ingress_security_groups = []
+  egress_cidr_blocks      = var.egress_cidr_blocks
+  ingress_ports           = module.sg-ports.ingress_master_ports
+  sg_name_prefix          = format("%s-%s", var.name_prefix, "-master")
+  egress_protocol         = "all"
+  ingress_protocol        = "tcp"
+}
+
+module "aws-emr-sg-core" {
+  source                  = "git::git@github.com:Datatamer/terraform-aws-security-groups.git?ref=1.0.0"
+  vpc_id                  = var.vpc_id
+  ingress_cidr_blocks     = var.ingress_cidr_blocks
+  ingress_security_groups = []
+  egress_cidr_blocks      = var.egress_cidr_blocks
+  ingress_ports           = module.sg-ports.ingress_core_ports
+  sg_name_prefix          = format("%s-%s", var.name_prefix, "-core")
+  egress_protocol         = "all"
+  ingress_protocol        = "tcp"
+}
+
+module "aws-emr-sg-service-access" {
+  source              = "git::git@github.com:Datatamer/terraform-aws-security-groups.git?ref=1.0.0"
+  vpc_id              = var.vpc_id
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+  egress_cidr_blocks  = var.egress_cidr_blocks
+  ingress_ports       = module.sg-ports.ingress_service_access_ports
+  sg_name_prefix      = format("%s-%s", var.name_prefix, "-service-access")
+  egress_protocol     = "all"
+  ingress_protocol    = "tcp"
+}
---
+++ examples/static-hbase-logs/outputs.tf
@@ -0,0 +1,20 @@
+output "logs-bucket" {
+  value = module.emr-logs-bucket
+}
+
+output "rootdir-bucket" {
+  value = module.emr-rootdir-bucket
+}
+
+output "cluster" {
+  value = module.emr-hbase
+}
+
+output "ec2-key" {
+  value = module.emr_key_pair
+}
+
+output "private-key" {
+  value     = tls_private_key.emr_private_key.private_key_pem
+  sensitive = true
+}
