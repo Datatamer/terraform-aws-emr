e25c9acc1b797c4353373dc083718cf9938b7956
Federico Vazquez <federico.vazquez@caylent.com>
2022-04-12 12:02:31 -0300

    Updates cloudwatch install file location.

--- examples/static-spark-encrypted/cloudwatch.tf
+++ examples/static-spark-encrypted/cloudwatch.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_log_group" "tamr_log_group" {
 # Get current Region
 data "aws_region" "current" {}
 
-resource "local_file" "postfix_config" {
-  filename = "${path.module}/tempfiles/cloudwatch-install.sh"
-  content  = templatefile("../files/cloudwatch-install.tpl", { region = data.aws_region.current.name, endpoint = var.vpce_logs_endpoint_dnsname, log_group = aws_cloudwatch_log_group.tamr_log_group.name })
+resource "local_file" "cloudwatch-install" {
+  filename = "${path.module}/tempfiles/cloudwatch-install-spark.sh"
+  content  = templatefile("../files/cloudwatch-install-spark.tpl", { region = data.aws_region.current.name, endpoint = var.vpce_logs_endpoint_dnsname, log_group = aws_cloudwatch_log_group.tamr_log_group.name })
 }
