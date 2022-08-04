38e94f21b4fc624fe83681ef7db12c49639b54e7
Dan Souza <dan.souza@tamr.com>
2022-08-04 11:38:56 -0400

    adjusts AWS provider constraints to allow newer versions

--- modules/aws-emr-cluster/versions.tf
+++ modules/aws-emr-cluster/versions.tf
@@ -1,6 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
   required_providers {
-    aws = ">= 2.45.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.36, !=4.0.0, !=4.1.0, !=4.2.0, !=4.3.0, !=4.4.0, !=4.5.0, !=4.6.0, !=4.7.0, !=4.8.0"
+    }
   }
 }
