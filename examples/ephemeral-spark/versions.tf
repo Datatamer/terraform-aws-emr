3251630d74fc601c65dda06292dac33dfd9b33dc
creynolds15 <colin.reynolds@tamr.com>
2024-03-15 07:49:14 -0700

    chore: add tls provider to further versions

--- examples/ephemeral-spark/versions.tf
+++ examples/ephemeral-spark/versions.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">=4.0.0"
