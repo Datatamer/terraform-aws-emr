22f40c5e2c2bf9dac4c080a9fd44d70d78fad209
creynolds15 <colin.reynolds@tamr.com>
2024-03-15 09:48:15 -0700

    chore: add local to all examples

--- examples/static-spark/versions.tf
+++ examples/static-spark/versions.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.1"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 3.0.0"
